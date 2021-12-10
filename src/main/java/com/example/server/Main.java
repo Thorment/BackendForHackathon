ef9448977e1c70f56bc8d5bb329435a98cb7f89a
Dikra <dino-kraft@live.de>
2021-12-10 10:05:54 +0100

    git 2.0

--- src/main/java/com/example/server/Main.java
+++ src/main/java/com/example/server/Main.java
@@ -11,10 +11,10 @@ public class Main {
 		Item jan = new Item("Jan");
 		Item dino = new Item("Dino");
 		Item kristin = new Item("Kristin");
-		Item ändy = new Item("Ändy");
-		Item gäralt = new Item("Gäralt");
+		Item andy = new Item("Andy");
+		Item geralt = new Item("Geralt");
 
-		gitKing(dino);
+		gitgud(dino);
 
 
 	}
@@ -22,13 +22,11 @@ public class Main {
 	public static void dance(Item item) {
 		System.out.println(item.getName() + " tanzt für dich! yeah!" );
 	}
-	public static void gitKing(Item item) {
+	public static void gitgud(Item item) {
 		System.out.println(item.getName() + " Hat git integriert!" );
 	}
-
-
-	public static void typischÄndy(){
-		System.out.println("Typisch Ändy VS. Eclipse");
+	public static void typischAndy(){
+		System.out.println("Typisch Andy VS. Eclipse");
 	}
 	public static void catchSpannungsabfall(Item item){
 		System.out.println(item.getName()+" holt den Spannungsabfalleimer und geht sammeln");
