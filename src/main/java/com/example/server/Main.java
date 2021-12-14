cb6de2b993d5ad616dfae597bbe0ea4273049c94
Krissxy <krissyx33@hotmail.de>
2021-12-14 09:37:15 +0100

    fixing

--- src/main/java/com/example/server/Main.java
+++ src/main/java/com/example/server/Main.java
@@ -1,4 +1,4 @@
-package com.example.server.application;
+package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
